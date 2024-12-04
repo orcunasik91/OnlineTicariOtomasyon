8d7c4479a1c395c3fa1a54168d4123abe02f672b
Orçun Aşık <asikorcun@gmail.com>
2024-12-04 22:12:14 +0300

    Modellerimizi veri tabanına yansıtmak için context sınıfı ve migration oluşturuldu.

---
+++ OnlineTicariOtomasyon.WebUI/Models/Entities/Context.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace OnlineTicariOtomasyon.WebUI.Models.Entities
+{
+    public class Context : DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Kategori> Kategoris { get; set; }
+        public DbSet<Urun> Uruns { get; set; }
+        public DbSet<Departmanlar> Departmanlars { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+        public DbSet<Fatura> Faturas { get; set; }
+        public DbSet<FaturaKalem> FaturaKalems { get; set; }
+        public DbSet<Cari> Caris { get; set; }
+        public DbSet<Gider> Giders { get; set; }
+        public DbSet<SatisHareket> SatisHarekets { get; set; }
+    }
+}
