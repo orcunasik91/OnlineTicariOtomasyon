771c53b57e73aa18941789f0e2c6beb1ebdb1e17
Orçun Aşık <asikorcun@gmail.com>
2024-12-10 20:03:19 +0300

    AdminLTE teması eklendi

---
+++ OnlineTicariOtomasyon.WebUI/Controllers/UrunDetayController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.WebUI.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+        // GET: UrunDetay
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
