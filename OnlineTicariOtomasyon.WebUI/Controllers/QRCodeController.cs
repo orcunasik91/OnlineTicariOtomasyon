f770b061e862e7aa58412d69b565647b1b35cbb1
Orçun Aşık <asikorcun@gmail.com>
2024-12-18 23:27:37 +0300

    QRCode işlemleri tamamlandı

---
+++ OnlineTicariOtomasyon.WebUI/Controllers/QRCodeController.cs
@@ -0,0 +1,33 @@
+﻿using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.WebUI.Controllers
+{
+    public class QRCodeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string kod)
+        {
+            using(MemoryStream ms = new MemoryStream())
+            {
+                QRCodeGenerator kodUret = new QRCodeGenerator();
+                QRCodeGenerator.QRCode karekod = kodUret.CreateQrCode(kod, QRCodeGenerator.ECCLevel.Q);
+                using(Bitmap resim = karekod.GetGraphic(10))
+                {
+                    resim.Save(ms, ImageFormat.Png);
+                    ViewBag.KarekodImage = "data:Content/images/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return View();
+        }
+    }
+}
