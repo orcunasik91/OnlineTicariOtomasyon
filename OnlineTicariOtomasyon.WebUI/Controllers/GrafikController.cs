6535096d7f106815c683bf21b720f04e04066f77
Orçun Aşık <asikorcun@gmail.com>
2024-12-20 21:03:04 +0300

    Grafikler bölümü tamamlandı

---
+++ OnlineTicariOtomasyon.WebUI/Controllers/GrafikController.cs
@@ -0,0 +1,34 @@
+﻿using OnlineTicariOtomasyon.WebUI.Models;
+using OnlineTicariOtomasyon.WebUI.Models.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.WebUI.Controllers
+{
+    public class GrafikController : Controller
+    {
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public List<UrunListesi> UrunListesi()
+        {
+            List<UrunListesi> urunListesi = new List<UrunListesi>();
+            using (Context context = new Context())
+            {
+                urunListesi = context.Uruns.Select(u => new UrunListesi
+                {
+                    urunAd = u.UrunAd,
+                    stok = u.Stok
+                }).ToList();
+            }
+            return urunListesi;
+        }
+        public ActionResult VisualizeUrunResult()
+        {
+            return Json(UrunListesi(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ OnlineTicariOtomasyon.WebUI/Models/UrunListesi.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineTicariOtomasyon.WebUI.Models
+{
+    public class UrunListesi
+    {
+        public string urunAd { get; set; }
+        public short stok { get; set; }
+    }
+}
